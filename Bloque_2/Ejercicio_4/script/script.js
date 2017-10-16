07a85bc3facd4d0366165e6f9fe25c885865f100
charlestone <carlosdr2332@gmail.com>
2017-10-16 11:58:04 +0200

    Js y rutas
    Arreglo las rutas a archivos en css e intento que aparezca una ventana modal (sin éxito)

--- Bloque_2/Ejercicio_4/script/script.js
+++ Bloque_2/Ejercicio_4/script/script.js
@@ -1,7 +1,10 @@
-document.getElementsByTagName('body').addEventListener();
+var body = document.getElementsByTagName('body')[0];
+var modal = document.getElementsByClassName('modal')[0];
+body.addEventListener("load",showModal);
 
-function openModal() {
-	;
-	;
-	;
+//body.addEventListener("unload",);
+
+
+function showModal() {
+	modal.style.display = "block";
 }
