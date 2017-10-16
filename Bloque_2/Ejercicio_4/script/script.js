82c5ed1c4a9772aaf761b1bb681def2adb35e6ad
hurtadoisabel <32375030+hurtadoisabel@users.noreply.github.com>
2017-10-16 17:22:07 +0200

    payment
    añado el JavaScript
    metodo de pago dinámico

---
+++ Bloque_2/Ejercicio_4/script/script.js
@@ -0,0 +1,33 @@
+
+var payment = document.getElementById('payment');
+var transferencia = document.getElementById('transferencia');
+var tarjeta = document.getElementById('tarjeta');
+var paypal = document.getElementById('paypal');
+
+payment.addEventListener("change", metodospago);
+
+
+function metodospago(){
+
+	switch(payment.value){
+
+		case 'transferencia':
+			paypal.style.display = "none";
+			tarjeta.style.display = "none";
+			transferencia.style.display = "list-item";
+			break;	
+
+		case 'tarjeta':
+			paypal.style.display = "none";
+			transferencia.style.display = "none";
+			tarjeta.style.display = "list-item";	
+			break;
+
+		case 'paypal':
+			transferencia.style.display = "none";
+			tarjeta.style.display = "none";
+			paypal.style.display = "list-item";	
+			break;
+	}
+		
+}
