9eb9337dc955893106299ce8695f770b878d3c5f
hurtadoisabel <32375030+hurtadoisabel@users.noreply.github.com>
2017-10-16 20:19:18 +0200

    validaciones
    validacion del email
    validacion del nombre y apellido

--- Bloque_2/Ejercicio_4/script/script.js
+++ Bloque_2/Ejercicio_4/script/script.js
@@ -9,11 +9,39 @@ payment.addEventListener("change", metodospago);
 
 function validateForm() {
     var x = document.forms["formulario"]["email"].value;
-    if (x == "") {
-        alert("e-mail must be filled out");
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
         return false;
     }
+
+    var y = document.forms["formulario"]["NameLName"].value;
+    if (y == "") {
+        alert("Name and Last Name must be filled out");
+        return false;
+    } 
+
+    var a = document.forms["formulario"]["adress"].value;
+    if (a == "") {
+        alert("Adress must be filled out");
+        return false;
+    }
+
+    var b = document.forms["formulario"]["metodospago"].value;
+	if (b == "") {
+        alert("Payment method must be filled out");
+        return false;
+    }
+    
+   var c = document.getElementById("myCheck").checked;
+   if(c == false){
+		alert('You must agree to the terms first.');
+   }
+
 }
+
 
 function metodospago(){
 
