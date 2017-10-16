29d8321f1b0b789a22d9176f9b7a76643cf8144e
charlestone <carlosdr2332@gmail.com>
2017-10-16 19:00:37 +0200

    modal y js
    cambio el color de la cruz cuando pasas el raton a rojo y organizo el js

--- Bloque_2/Ejercicio_4/script/script.js
+++ Bloque_2/Ejercicio_4/script/script.js
@@ -1,7 +1,7 @@
 var modal = document.getElementsByClassName('modal')[0];
 var close = document.getElementsByClassName('close')[0];
-close.addEventListener("click", hideModal);
 window.addEventListener("load", showModal);
+close.addEventListener("click", hideModal);
 
 function showModal() {
 	modal.style.display = "block";
