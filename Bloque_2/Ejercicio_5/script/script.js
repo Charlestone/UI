70e67e6093f1d8eec9f2dd972d9e4b2374a154c4
hurtadoisabel <32375030+hurtadoisabel@users.noreply.github.com>
2017-10-25 23:28:47 +0200

    descripcion videos
    boton + muestra la info y si pulso otra vez se oculta

--- Bloque_2/Ejercicio_5/script/script.js
+++ Bloque_2/Ejercicio_5/script/script.js
@@ -3,6 +3,12 @@
 $(document).ready(function(){
     var likes =$("p[id=likes]").text();
     var numlikes = Number(likes);
+
+    var oculto1 = true;
+    var oculto2 = true;
+    var oculto3 = true;
+
+
     $("img[name=corazon]").click(function () {
     numlikes++;
     numlikes.toString();
@@ -17,24 +23,48 @@ $(document).ready(function(){
 
 
     });
-    $("img[name=mas1]").click(function(){
-        $(".posdescripcion1").show();
+
+     $("img[name=mas1]").click(function(){
+        if(oculto1 == false){
+        $(".posdescripcion1").hide();
+        oculto1 = true;
+        } else { 
+            $(".posdescripcion1").show();
+            oculto1=false;
+        }
+
     });
 
+    
+   
+
+
     $("img[name=mas2]").click(function(){
-        $(".posdescripcion2").show();
+
+        if(oculto2 == false){
+        $(".posdescripcion2").hide();
+        oculto2 = true;
+        } else { 
+            $(".posdescripcion2").show();
+            oculto2=false;
+        }
+
     });
 
     $("img[name=mas3]").click(function(){
-        $(".posdescripcion3").show();
+        if(oculto3 == false){
+        $(".posdescripcion3").hide();
+        oculto3 = true;
+        } else { 
+            $(".posdescripcion3").show();
+            oculto3=false;
+        }
+
     });
 
 
-   /* $(window).click(function(){
-        if($('#descripcion').attr("display")!="none"){
-        $('#descripcion').hide();
-        }
-    });*/
+
+
 });
 
 
