fea93cf4e3461d3a8d51031aa6cb124fb2d1e169
hurtadoisabel <32375030+hurtadoisabel@users.noreply.github.com>
2017-10-25 23:13:07 +0200

    descripciones videos
    Añado la descripciones de cada video para que al hacer click en el + se muestren. Falla que se agrande la imagen.

--- Bloque_2/Ejercicio_5/script/script.js
+++ Bloque_2/Ejercicio_5/script/script.js
@@ -1,19 +1,44 @@
 
 
-/*var numlikes = parseInt(likes);*/
-
 $(document).ready(function(){
     var likes =$("p[id=likes]").text();
     var numlikes = Number(likes);
     $("img[name=corazon]").click(function () {
-    //$("img").click(function () {
-    //$("#idimagen").click(function () {
-    //$("img[class=claseimagen]").click(function () {
-    //$(".claseimagen").click(function () {
-    window.alert(likes);
-    window.alert(numlikes);
     numlikes++;
     numlikes.toString();
     $("p[id=likes]").text(numlikes);
     });
+    var shares = $("p[id=shares]").text();
+    var numshares = Number(shares);
+    $("img[name=compartir]").click(function(){
+      numshares++;
+      numshares.toString();
+      $("p[id=shares]").text(numshares);
+
+
+    });
+    $("img[name=mas1]").click(function(){
+        $(".posdescripcion1").show();
+    });
+
+    $("img[name=mas2]").click(function(){
+        $(".posdescripcion2").show();
+    });
+
+    $("img[name=mas3]").click(function(){
+        $(".posdescripcion3").show();
+    });
+
+
+   /* $(window).click(function(){
+        if($('#descripcion').attr("display")!="none"){
+        $('#descripcion').hide();
+        }
+    });*/
 });
+
+
+
+
+    
+ 
