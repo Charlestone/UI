a578e84fbeb518dce675c5321af7ce80657d247e
hurtadoisabel <32375030+hurtadoisabel@users.noreply.github.com>
2017-10-28 21:11:05 +0200

    Ejercicio 6
    Estan ya las imagenes y se dan la vuelta.

---
+++ Bloque_2/Ejercicio_6/script/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	/*$('#my-memorygame').quizyMemoryGame({
+	itemWidth: 156,
+	itemHeight: 156, 
+	itemsMargin:40, 
+	colCount:5, 
+	animType:'flip', 
+	flipAnim:'tb', 
+	animSpeed:250, 
+	resultIcons:true
+
+	});*/
+
+	$(".carta").flip({
+		trigger: 'click'
+
+
+	});
+});
