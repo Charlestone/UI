465b76f4001b2a535930d551be5b935201d5d6ef
charlestone <carlosdr2332@gmail.com>
2017-10-28 11:41:24 +0200

    validacion modal
    Dejo casi terminada la validacion de los campos de la ventana modal. Falta comrpobar las expresiones regulares

--- Bloque_2/Ejercicio_4/script/script.js
+++ Bloque_2/Ejercicio_4/script/script.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
   $("#body").ready(function(){
     //El siguiente par de lineas esta puesto por firefox
-    $("#languages").val("choose");
-    $("#languages").val("choose");
     $(".modal").show();
 
   });
-  
+  var errors = "";
+  var email = /[a-z0-9]+@([a-z0-9]+\.)+[a-z]{2,4}/;
+  var password = /[a-z0-9]{1,8}/;
   $("#login").click(function(){
-    $(".modal").hide();
-    $("input[name=email]").val($("input[name=emailm]").val());
-    $("input[name=password]").val($("input[name=passwordm]").val());
+    if (!email.test($("#modal-email").val())) {
+      errors+= "Email inválido.\n";
+    }
+    if (!password.test($("#modal-password").val())) {
+      errors+= "Contraseña inválida.\n";
+    }
+    if (!errors) {
+      $(".modal").hide();
+      $("input[name=email]").val($("input[name=emailm]").val());
+      $("input[name=password]").val($("input[name=passwordm]").val());  
+    } else {
+      alert(errors + "Por favor, introduzca de nuevo los campos inválidos.");
+    }
+      
   });
 
   $("#payment").change(function() {
