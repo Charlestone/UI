d7b047bf3136b649cce1fb75247e7f6f4e6b6871
charlestone <carlosdr2332@gmail.com>
2017-10-29 19:49:07 +0100

    botones save y reset
    termino las funcionalidades de los botones save y reset

--- Bloque_2/Ejercicio_4/script/script.js
+++ Bloque_2/Ejercicio_4/script/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         $("#language").val("español");
         $("input[type!=button]").val("");
         $("input[type=checkbox]").prop('checked', false);
-        //document.cookie = "usern; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        //document.cookie = "user1=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
         //alert(document.cookie.split(";"));
     });
     /* Variables */
@@ -20,6 +20,9 @@ $(document).ready(function() {
     var actuser = "";
     var actcookie = "";
     var pay = 0;
+    var cambios = "";
+    var forms;
+    var photo = false;
     /* Funcion para guardar cookies */
     function setCookie(cname, cvalue) {
         var d = new Date();
@@ -33,7 +36,7 @@ $(document).ready(function() {
         if (cookies.length) {
             for (var i = 0; i < cookies.length; i++) {
                 if (cookies[i]) {
-                    var forms = cookies[i].match(/\$.+?\#/g);
+                    forms = cookies[i].match(/\$.+?\#/g);
                     if (forms[4].substr(1, forms[4].length - 2) == email) {
                         actcookie = cookies[i];
                         actuser = actcookie.match(/^\w*\d*=/);
@@ -58,7 +61,7 @@ $(document).ready(function() {
         if (!errors) {
             /*Comprueba si el usuario tiene cookie*/
             if (checkCookie($("#modal-email").val())) {
-                var forms = actcookie.match(/\$.+?\#/g);
+                forms = actcookie.match(/\$.+?\#/g);
                 /* Si la contraseña es correcta */
                 if (forms[1].substr(1, forms[1].length - 2) == $("#modal-password").val()) {
                     $("input[name=username]").val(forms[0].substr(1, forms[0].length - 2));
@@ -86,9 +89,6 @@ $(document).ready(function() {
                             $(this).attr("hasBeenClicked", "true");
                             break;
                     }
-                    if (forms.length == 13) {
-                        $("input[name=foto]").val(forms[12].substr(1, forms[12].length - 2));
-                    }
                     switch ($("#payment").val()) {
                         case 'transferencia':
                             $("#paypal").hide();
@@ -108,7 +108,7 @@ $(document).ready(function() {
                             $("#paypal").show();
                             break;
                     }
-                    $(".modal").hide();                    
+                    $(".modal").hide();
                 } else {
                     /* Si la contraseña no es correcta */
                     alert("Wrong password\nPlease enter a valid one.\n");
@@ -128,7 +128,14 @@ $(document).ready(function() {
     });
     /* Si se hace click en el enlace a Paypal */
     $("#pay").click(function() {
+        if ($(this).attr("hasBeenClicked") == "true") {
+            pay = 1;
+        }
         $(this).attr("hasBeenClicked", "true");
+    });
+    /* Si se cambia la foto*/
+    $("input[name=foto]").change(function() {
+        photo = true;
     });
     /* Si se hace click en el boton guardar */
     $("input[name=guardar]").click(function() {
@@ -200,8 +207,6 @@ $(document).ready(function() {
             case 'paypal':
                 if ($("#pay").attr("hasBeenClicked") != "true") {
                     errors += "Invalid payment method.\n";
-                } else {
-                    pay = 1;
                 }
                 break;
         }
@@ -234,10 +239,90 @@ $(document).ready(function() {
                     user += "$" + $("input[name=Acnumber]").val() + "#";
                     user += "$" + $("input[name=Bank]").val() + "#";
                     break;
+                case 'paypal':
+                    user += "";
+                    user += "";
+                    user += "";
+                    user += "";
+                    break;
             }
-            user += "$" + $("input[name=foto]").val() + "#";
+            checkCookie($("input[name=email]").val());
             setCookie(actuser, user);
-            //alert(document.cookie);
+            /* Se comprueba en que campos se han producido cambios */
+            if ($("input[name=username]").val() != forms[0].substr(1, forms[0].length - 2) && forms[0]) {
+                cambios += "Username\n";
+            }
+            if ($("input[name=password]").val() != forms[1].substr(1, forms[1].length - 2) && forms[1]) {
+                cambios += "Password\n";
+            }
+            if ($("input[name=NameLName]").val() != forms[2].substr(1, forms[2].length - 2) && forms[2]) {
+                cambios += "Name & Last Name\n";
+            }
+            if ($("input[name=Birthday]").val() != forms[3].substr(1, forms[3].length - 2) && forms[3]) {
+                cambios += "Birthday\n";
+            }
+            if (photo) {
+                cambios += "Photo\n";
+                photo = false;
+            }
+            if ($("#language").val() != forms[5].substr(1, forms[5].length - 2) && forms[5]) {
+                cambios += "Language\n";
+            }
+            if ($("input[name=address]").val() != forms[6].substr(1, forms[6].length - 2) && forms[6]) {
+                cambios += "Address\n";
+            }
+            if ($("#payment").val() != forms[7].substr(1, forms[7].length - 2) && forms[7]) {
+                cambios += "Payment method\n";
+                switch ($("#payment").val()) {
+                    case 'tarjeta':
+                        cambios += "Number\nValid Thru\nCode\n";
+                        break;
+
+                    case 'transferencia':
+                        cambios += "Beneficiary's Name\nSurname\nAccount Number\nBank\n";
+                        break;
+                }
+            } else {
+                switch ($("#payment").val()) {
+                    case 'transferencia':
+                        if ($("input[name=Beneficiary]").val() != forms[8].substr(1, forms[8].length - 2) && forms[8]) {
+                            cambios += "Beneficiary's Name\n";
+                        }
+                        if ($("input[name=Surname]").val() != forms[9].substr(1, forms[9].length - 2) && forms[9]) {
+                            cambios += "Surname\n";
+                        }
+                        if ($("input[name=Acnumber]").val() != forms[10].substr(1, forms[10].length - 2) && forms[10]) {
+                            cambios += "Account Number\n";
+                        }
+                        if ($("input[name=Bank]").val() != forms[11].substr(1, forms[11].length - 2) && forms[11]) {
+                            cambios += "Bank\n";
+                        }
+                        break;
+
+                    case 'tarjeta':
+                        if ($("input[name=number]").val() != forms[8].substr(1, forms[8].length - 2) && forms[8]) {
+                            cambios += "Number\n";
+                        }
+                        if ($("input[name=valid]").val() != forms[9].substr(1, forms[9].length - 2) && forms[9]) {
+                            cambios += "Valid Thru\n";
+                        }
+                        if ($("input[name=code]").val() != forms[10].substr(1, forms[10].length - 2) && forms[10]) {
+                            cambios += "Code\n";
+                        }
+                        break;
+                    case 'paypal':
+                        if (pay) {
+                            cambios += "Paypal";
+                        }
+                        break;
+                }
+            }
+            
+            /* Se muestran los cambios */
+            if (cambios) {
+                alert("The following changes have been saved:\n" + cambios);
+                cambios = "";
+            }
         } else {
             /* Si los hay */
             if (errors) {
@@ -248,6 +333,80 @@ $(document).ready(function() {
             terms = "";
         }
     });
+    /* Si se hace click en el boton reset */
+    $("input[name=borrar]").click(function() {
+        /* Si hay cookie, restauramos los valores de esta*/
+        
+        if (checkCookie($("#modal-email").val())) {
+            $("input[name=username]").val(forms[0].substr(1, forms[0].length - 2));
+            $("input[name=password]").val(forms[1].substr(1, forms[1].length - 2));
+            $("input[name=NameLName]").val(forms[2].substr(1, forms[2].length - 2));
+            $("input[name=Birthday]").val(forms[3].substr(1, forms[3].length - 2));
+            $("input[name=email]").val(forms[4].substr(1, forms[4].length - 2));
+            $("#language").val(forms[5].substr(1, forms[5].length - 2));
+            $("input[name=address]").val(forms[6].substr(1, forms[6].length - 2));
+            $("#payment").val(forms[7].substr(1, forms[7].length - 2));
+            switch ($("#payment").val()) {
+                case 'transferencia':
+                    $("input[name=Beneficiary]").val(forms[8].substr(1, forms[8].length - 2));
+                    $("input[name=Surname]").val(forms[9].substr(1, forms[9].length - 2));
+                    $("input[name=Acnumber]").val(forms[10].substr(1, forms[10].length - 2));
+                    $("input[name=Bank]").val(forms[11].substr(1, forms[11].length - 2));
+                    break;
+
+                case 'tarjeta':
+                    $("input[name=number]").val(forms[8].substr(1, forms[8].length - 2));
+                    $("input[name=valid]").val(forms[9].substr(1, forms[9].length - 2));
+                    $("input[name=code]").val(forms[10].substr(1, forms[10].length - 2));
+                    break;
+                case 'paypal':
+                    $(this).attr("hasBeenClicked", "true");
+                    break;
+            }
+            switch ($("#payment").val()) {
+                case 'transferencia':
+                    $("#paypal").hide();
+                    $("#tarjeta").hide();
+                    $("#transferencia").show();
+                    break;
+
+                case 'tarjeta':
+                    $("#paypal").hide();
+                    $("#transferencia").hide();
+                    $("#tarjeta").show();
+                    break;
+
+                case 'paypal':
+                    $("#tarjeta").hide();
+                    $("#transferencia").hide();
+                    $("#paypal").show();
+                    break;
+            }
+            $("input[name=foto]").val("");
+        } else {
+            /* Si no hay cookie, dejamos solo los valores que el usuario 
+            ha introducido en la ventana modal */
+            $("input[name=username]").val("");
+            $("input[name=NameLName]").val("");
+            $("input[name=Birthday]").val("");
+            $("input[name=foto]").val("");
+            $("input[name=address]").val("");
+            $("input[name=number]").val("");
+            $("input[name=valid]").val("");
+            $("input[name=code]").val("");
+            $("input[name=Beneficiary]").val("");
+            $("input[name=Surname]").val("");
+            $("input[name=Acnumber]").val("");
+            $("input[name=Bank]").val("");
+            $("#language").val("español");
+            $("#payment").val("choose");
+            $("#tarjeta").hide();
+            $("#transferencia").hide();
+            $("#paypal").hide();
+            $("input[type=checkbox]").prop('checked', false);
+        }
+    });
+
     /* Si se cambia el metodo de pago */
     $("#payment").change(function() {
         $("#tarjeta input").val("");
