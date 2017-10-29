2d1df7b649f0d58dbc3106fcf4df27493c235658
hurtadoisabel <32375030+hurtadoisabel@users.noreply.github.com>
2017-10-29 19:49:42 +0100

    timer

--- Bloque_2/Ejercicio_6/script/script.js
+++ Bloque_2/Ejercicio_6/script/script.js
@@ -17,10 +17,28 @@ $(document).ready(function(){
 
 	});
 
+	var imagenes = new Array("./img/corzo.png", "./img/ardilla.png", "./img/dalmata.png",
+		"./img/delfin.png", "./img/focas.png", "./img/gato.png", "./img/corzo.png", "./img/pajaro.png",
+		"./img/pez.png", "./img/tucan.png", "./img/zorro.png", "./img/corzo.png");
 
-	function RandomFunction(MaxValue, MinValue) {
-		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+	var milisegundos = 2000;
+	var siguiente = 0;
+
+	function temporizador(){
+		$(".imagenes").attr("src", imagenes[siguiente]);
+		$(".imagenes")slideDown(1000).delay(1000).fadeIn(1000);
+		$(".imagenes").fadeOut(1500).delay(500).slideUp(1000, function(){
+			timer = setTimeout('temporizador()', milisegundos);
+		}
+
 	}
+
+
+	/*var tiempodejuego = $("#tiempodejuego").val();
+	setTimeout(function() {
+      
+    }, $(tiempodejuego);
+*/
 
 
 });
