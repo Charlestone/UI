ea795217970fe60ca231e77616fd1c3225ecc382
charlestone <carlosdr2332@gmail.com>
2017-12-07 13:57:46 +0100

    base no reg
    Añado fuentes, imágenes y demás carpetas. Header de no registrados casi completo, solo queda la ventana modal

---
+++ ProyectoFinal/Páginas base/noreg/script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    /* Para el logo */
+    $("#logo").click(function(){
+        window.location.href= './basenr.html';
+    });
+    /* Para el menu desplegable */
+    $(".menu").click(function() {
+        $(".dropdown-content").toggle();
+    });
+    /* Para el boton de iniciar sesion */
+});
